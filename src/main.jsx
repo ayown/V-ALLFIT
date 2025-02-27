3d88b36b5a9bc540efa91061d44858122f1cc960
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2025-02-28 00:35:39 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode className='p-2.5'>
+  <StrictMode>
     <App />
   </StrictMode>,
 )
