0159d81e88a3602a55fdb291fc4938264b89d369
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2025-02-28 00:33:37 +0530

    updated everything

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -13,7 +13,7 @@ function Header(props) {
     return (
         <div className='flex flex-col gap-4'>
             <div className='flex items-center justify-center gap-2'>
-                <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400'>{index}</p>
+                <p className='text-3xl sm:text-4xl md:text-5xl font-semibold text-blue-800'>{index}</p>
                 <h4 className='text-xl sm:text-2xl md:text-3xl'>{title}</h4>
             </div>
             <p className='text-sm sm:text-base mx-auto'>{description}</p>
@@ -57,7 +57,7 @@ export default function Generator(props) {
     return (
         <SectionWrapper id={'generate'} header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
             <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."} />
-            <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+            <div className='grid grid-cols-2 p-3.5 sm:grid-cols-4 gap-4 mb-5'>
                 {Object.keys(WORKOUTS).map((type, typeIndex) => {
                     return (
                         <button onClick={() => {
@@ -70,27 +70,30 @@ export default function Generator(props) {
                 })}
             </div>
             <Header index={'02'} title={'Lock on targets'} description={"Select the muscles judged for annihilation."} />
-            <div className='bg-slate-950  border border-solid border-blue-400 rounded-lg flex flex-col'>
-                <button onClick={toggleModal} className='relative p-3 flex items-center justify-center'>
-                    <p className='capitalize'>{muscles.length == 0 ? 'Select muscle groups' : muscles.join(' ')}</p>
-                    <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
-                </button>
-                {showModal && (
-                    <div className='flex flex-col px-3 pb-3'>
-                        {(poison === 'individual' ? WORKOUTS[poison] : Object.keys(WORKOUTS[poison])).map((muscleGroup, muscleGroupIndex) => {
-                            return (
-                                <button onClick={() => {
-                                    updateMuscles(muscleGroup)
-                                }} key={muscleGroupIndex} className={'hover:text-blue-400 duration-200 ' + (muscles.includes(muscleGroup) ? ' text-blue-400' : ' ')}>
-                                    <p className='uppercase'>{muscleGroup.replaceAll('_', ' ')}</p>
-                                </button>
-                            )
-                        })}
-                    </div>
-                )}
+            <div className='p-3.5 mb-6.5'>
+                <div className='bg-slate-950  border border-solid border-blue-400 rounded-lg  flex flex-col'>
+                    <button onClick={toggleModal} className='relative p-3 flex items-center justify-between'>
+                        <p className='capitalize'>{muscles.length == 0 ? 'Select muscle groups' : muscles.join(' ')}</p>
+                        <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
+                    </button>
+                    {showModal && (
+                        <div className='flex flex-col px-3 pb-3'>
+                            {(poison === 'individual' ? WORKOUTS[poison] : Object.keys(WORKOUTS[poison])).map((muscleGroup, muscleGroupIndex) => {
+                                return (
+                                    <button onClick={() => {
+                                        updateMuscles(muscleGroup)
+                                    }} key={muscleGroupIndex} className={'hover:text-blue-400 duration-200 ' + (muscles.includes(muscleGroup) ? ' text-blue-400' : ' ')}>
+                                        <p className='uppercase'>{muscleGroup.replaceAll('_', ' ')}</p>
+                                    </button>
+                                )
+                            })}
+                        </div>
+                    )}
+                </div>
             </div>
+            
             <Header index={'03'} title={'Become Juggernaut'} description={"Select your ultimate objective."} />
-            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
+            <div className='grid p-3.5 grid-cols-1 sm:grid-cols-3 mb-4 gap-4'>
                 {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
                     return (
                         <button onClick={() => {
